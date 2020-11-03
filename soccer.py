e319697a5de468df935bc1bb0767d38d7d8e6b49
nicolai-b <nicobrumm@hotmail.com>
2020-11-03 09:55:10 +0100

    connect to sql

--- soccer.py
+++ soccer.py
@@ -1,1 +1,13 @@
+import numpy
+import pandas
 import sqlite3
+
+path = "C://Users//nicob//OneDrive//Dokumenter//Skole greier//Maskinlæring"
+database = path + 'database.sqlite'
+
+conn = sqlite3.connect(database)
+
+tables = pandas.read_sql("""SELECT *
+                        FROM sqlite_master
+                        WHERE type='table';""", conn)
+print(tables)

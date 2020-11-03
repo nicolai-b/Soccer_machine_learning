e58e32537b51c79086361d209e27c2d0f22d17e7
nicolai-b <nicobrumm@hotmail.com>
2020-11-03 10:11:47 +0100

    print list of countries

--- soccer.py
+++ soccer.py
@@ -2,12 +2,15 @@ import numpy
 import pandas
 import sqlite3
 
-path = "C://Users//nicob//OneDrive//Dokumenter//Skole greier//Maskinlæring"
+path = "C://Users//nicob//OneDrive//Dokumenter//Skole greier//Maskinlæring//"
 database = path + 'database.sqlite'
 
 conn = sqlite3.connect(database)
+#tables = pandas.read_sql("""SELECT *
+#                        FROM sqlite_master;""",conn)
+#                        WHERE type='table';""", conn)
+#print(tables)
 
-tables = pandas.read_sql("""SELECT *
-                        FROM sqlite_master
-                        WHERE type='table';""", conn)
-print(tables)
+countries = pandas.read_sql("""SELECT *
+                        FROM Country;""", conn)
+print(countries)
